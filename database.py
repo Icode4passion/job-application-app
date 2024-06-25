1a2a9c6edbe08bc2e2a6906411435ec5b7d7c189
Icode4passion <yogeerama@gmail.com>
2024-06-26 01:16:41 +0530

    add db file and modified app.py

---
+++ database.py
@@ -0,0 +1,23 @@
+import os
+from sqlalchemy import create_engine,text
+
+db_connection_string = os.environ['DATABASE_URL']
+engine = create_engine(db_connection_string)
+
+
+def load_jobs_from_db():
+   
+  with engine.connect() as conn:
+      result = conn.execute(text("select * from jobs"))
+      jobs = []
+      for row in result.all():
+          jobs.append(row._asdict())
+      return jobs
+
+
+    
+    
+
+
+ 
+
