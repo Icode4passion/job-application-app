c970d1a77bd4be5d6b60730a0d7aacb1f125b1c2
Icode4passion <yogeerama@gmail.com>
2024-06-26 01:33:55 +0530

    modified database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine,text
 
 db_connection_string = os.environ['DATABASE_URL']
-engine = create_engine(db_connection_string)
+engine = create_engine(db_connection_string,pool_size=10, max_overflow=20)
 
 
 def load_jobs_from_db():
